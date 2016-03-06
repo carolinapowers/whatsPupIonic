1d9ee6c045a592e0b951ddcc4c29796015fd05f9
Carolina Powers <carolinapoloni@gmail.com>
2016-03-05 22:54:27 -0500

    list items in place

--- www/app/clients/clients.controller.js
+++ www/app/clients/clients.controller.js
@@ -6,8 +6,11 @@ angular
     .module('whatsPupIonic')
     .controller('Clients', Clients);
 
-function Clients () {
+function Clients ($scope) {
     var vm = this;
+    $scope.$on('$ionicView.beforeEnter', function (event, viewData) {
+        viewData.enableBack = false;
+}); 
     
 }
 
