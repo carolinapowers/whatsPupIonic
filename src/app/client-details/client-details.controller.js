27ab1d058dbbba18e532b757b0cf93c308d08a65
Carolina Powers <carolinapoloni@gmail.com>
2016-03-10 18:50:48 -0500

    added action sheet on client details page

--- src/app/client-details/client-details.controller.js
+++ src/app/client-details/client-details.controller.js
@@ -6,11 +6,11 @@ angular
     .module('whatsPupIonic')
     .controller('ClientDetails', ClientDetails);
     
-ClientDetails.$inject=['CONST', '$stateParams','clients', '$firebaseObject'];
+ClientDetails.$inject=['CONST', '$stateParams','clients', '$firebaseObject', '$ionicActionSheet', '$timeout'];
  
 /** @ngInject */
     
-function ClientDetails (CONST, $stateParams, clients, $firebaseObject) {
+function ClientDetails (CONST, $stateParams, clients, $firebaseObject, $ionicActionSheet, $timeout) {
     var vm = this;
     vm.inputDisplayTitle = CONST.inputDisplayTitle;
    
@@ -19,7 +19,29 @@ function ClientDetails (CONST, $stateParams, clients, $firebaseObject) {
             .then(function (data) {
                 console.log(data);
                 vm.clientData = data; 
-        }); 
+        });
+    
+    vm.show = function() {
+
+   // Show the action sheet
+   var hideSheet = $ionicActionSheet.show({
+     buttons: [
+       { text: 'Add New Visit' },
+       { text: 'Edit' }
+     ],
+     destructiveText: 'Delete',
+     cancelText: 'Cancel',
+     cancel: function() {
+          // add cancel code..
+        },
+     buttonClicked: function(index) {
+       return true;
+     }
+   });
+
+  
+
+ };
 }
 
 })();
