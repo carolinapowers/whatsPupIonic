a8c951dd33c670d9d3f096fffde4ef985e966878
Carolina Powers <carolinapoloni@gmail.com>
2016-03-10 22:21:05 -0500

    app is sending email

--- src/app/new-visit/new-visit.controller.js
+++ src/app/new-visit/new-visit.controller.js
@@ -8,11 +8,9 @@ angular
     
 /** @ngInject */    
 
-function NewVisit (clients, $stateParams) {
+function NewVisit (clients, $stateParams, $http) {
     var vm = this;
-    vm.food = false;
-    vm.water= false;
-    vm.yesTreat = false;
+    vm.treats = false;
     console.log($stateParams);
     
     clients.getClient($stateParams.userId, $stateParams.clientId)
@@ -21,6 +19,105 @@ function NewVisit (clients, $stateParams) {
                 console.log(data);
                 vm.clientData = data; 
         }); 
+    
+     var currentdate = new Date();
+    
+     var time = (currentdate.getMonth() + 1) + "/" + currentdate.getDate() + "/" + currentdate.getFullYear() + " at " + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds(); //get time
+        
+        vm.sendEmail = function () {
+            $http({
+                method: "POST",
+                url: "https://mandrillapp.com/api/1.0/messages/send-template.json",
+                data: {
+                    'key': 'SjfF7oGr1BHLUnBlnSF20A',
+                    "template_name": "whatspup1",
+                    "template_content": [
+                        {
+                            "name": "example name",
+                            "content": "example content"
+                        }
+                    ],
+                    'message': {
+                        'from_email': 'whatspupupdate@gmail.com',
+                        'from_name': 'WhatsPup',
+                        'headers': {
+                            'Reply-To': 'whatspupupdate@gmail.com'
+                        },
+                        
+                        'subject': 'New Visit Update from WhatsPup',
+                        
+                        'to': [
+                            {
+                                'email': vm.clientData.email,
+                                'name': 'name',
+                                'type': 'to'
+                        }],
+                    
+                    "global_merge_vars": [
+                        {
+                            "name": "time",
+                           "content": time
+                        },
+                        {
+                            "name": "food",
+                            "content": vm.food
+                        },
+                        {
+                            "name": "water",
+                            "content": vm.water
+                        },
+                        {
+                            "name": "play",
+                            "content": vm.play
+                        },
+                        {
+                            "name": "treats",
+                            "content": vm.treats
+                        },
+                        {
+                            "name": "meds",
+                            "content": vm.meds
+                        },
+                        {
+                            "name": "mess",
+                            "content": vm.mess
+                        },
+                        {
+                            "name": "packages",
+                            "content": vm.packages
+                        },
+                        {
+                            "name": "mail",
+                            "content": vm.mail
+                        },
+                        {
+                            "name": "plants",
+                            "content": vm.plants
+                        },
+                        {
+                            "name": "other",
+                            "content": vm.other
+                        },
+                        {
+                            "name": "message",
+                            "content": vm.message
+                        },
+                        {
+                            "name": "image",
+                            "content": vm.image
+                        }
+                    ]
+                    }
+                }
+            })
+                .success(function (response) {
+                    alert('The visit has been saved. Thank you!'); // show success message
+                    console.log(response);
+                })
+                .error(function (response) {
+                    alert('There was a problem sending the visit.');
+                });
+        }
 }    
 
 
